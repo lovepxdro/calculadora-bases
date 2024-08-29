9bd76fcb3f0cf0f2bc892a1f0316fb54b5c013fe
pedrovrumvrum <143407084+lovepxdro@users.noreply.github.com>
2024-08-29 19:38:55 -0300

    Update main.c
    criei a função para converter para a base 8

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 // feito por pedro antônio (pafm)
-// versão 0.1 feita no dia 29/08/24 às 19:34 > criei a funçãode converter para a base 2
+// versão 0.1 feita no dia 29/08/24 às 19:34 > criei a função de converter para a base 2
+// versão 0.2 feita no dia 29/08/24 às 19:37 > criei a função de converter para a base 8
 
 #include <stdio.h>
 
@@ -10,12 +11,19 @@ void baseDois (int n){
     printf("%d", n%2);
 }
 
+void baseOito (int n){
+    if(n>1){
+        baseOito(n/8);
+    }
+    printf("%d", n%8);
+}
+
 int main()
 {
     int n;
     scanf("%d", &n);
     
-    baseDois(n);
+    baseOito(n);
 
     return 0;
 }
