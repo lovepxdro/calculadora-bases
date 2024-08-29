05073da421deb20c919f11245eb4dfc639a66f80
pedrovrumvrum <143407084+lovepxdro@users.noreply.github.com>
2024-08-29 19:40:30 -0300

    Update main.c
    criei a função de converter para a base 16

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 // feito por pedro antônio (pafm)
 // versão 0.1 feita no dia 29/08/24 às 19:34 > criei a função de converter para a base 2
 // versão 0.2 feita no dia 29/08/24 às 19:37 > criei a função de converter para a base 8
+// versão 0.3 feita no dia 29/08/24 às 19:39 > criei a função de converter para a base 16
 
 #include <stdio.h>
 
@@ -18,12 +19,45 @@ void baseOito (int n){
     printf("%d", n%8);
 }
 
+void baseDezesseis(int n) {
+    if (n >= 16) {
+        baseDezesseis(n / 16);
+    }
+
+    int resto = n % 16;
+
+    if (resto >= 10) {
+        switch (resto) {
+            case 10:
+                printf("A");
+                break;
+            case 11:
+                printf("B");
+                break;
+            case 12:
+                printf("C");
+                break;
+            case 13:
+                printf("D");
+                break;
+            case 14:
+                printf("E");
+                break;
+            case 15:
+                printf("F");
+                break;
+        }
+    } else {
+        printf("%d", resto);
+    }
+}
+
 int main()
 {
     int n;
     scanf("%d", &n);
     
-    baseOito(n);
+    baseDezesseis(n);
 
     return 0;
 }
